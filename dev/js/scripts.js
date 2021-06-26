7c29e02fe8e7b5bd850694aba6ce92c38ee75d26
Jessie0713 <r87713@gmail.com>
2021-06-27 03:01:39 +0800

    test

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,10 @@
-$('body').css('background-color' , '#122233')
+import Vue  from 'vue';
+$('body').css('background-color' , '#dddddd')
+new Vue({
+    el : '#app',
+    data : {
+       message : 'hello vue ++'
+    }
+  
+ })
+ 
