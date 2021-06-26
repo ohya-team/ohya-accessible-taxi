4a3263e3789282dad97de2351c0167030c21107d
yushinhe <robin124708476@gmail.com>
2021-06-27 01:27:01 +0800

    webpack嘗試

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        index: './dev/js/scripts.js',
+        bannerSlider: './dev/js/bannerSlider.js',
+        booking: './dev/js/booking.js',
+        forum: './dev/js/forum.js',
+        service: './dev/js/service.js',
+        travelForm: './dev/js/travelForm.js',
+    },// 入口文件
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: '[name].js'
+    },              // 出口文件
+    mode: 'development',
+    plugins: [
+        //全域載入jq
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        })
+    ],// 對應的插件
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    }, //解決vue jquery 路徑
+    // 服務器配置
+}
