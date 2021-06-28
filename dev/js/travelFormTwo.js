50ffb5cb7e2e9db4c4af59e752eab4a20a614cdf
gordonkuoss <gordonkuoss@gmail.com>
2021-06-29 07:00:46 +0800

    細微修改

--- dev/js/travelFormTwo.js
+++ dev/js/travelFormTwo.js
@@ -1,3 +1,9 @@
-document.getElementById("popbox-btn").addEventListener("click", function(){
+document.getElementById("popbox-btn").addEventListener("click", 
+function(){
   document.querySelector(".pop-up-box").classList.add('active')
+ });
+
+ document.getElementById("close-button").addEventListener("click", 
+function(){
+  document.querySelector(".pop-up-box").style.display = "none";
  });
