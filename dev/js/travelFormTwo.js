13e1b04eb5c3b194056e0ca166fb3d30e09e011d
yushinhe <robin124708476@gmail.com>
2021-06-28 14:11:07 +0800

    animate

--- dev/js/travelFormTwo.js
+++ dev/js/travelFormTwo.js
@@ -1,3 +1,3 @@
 document.getElementById("popbox-btn").addEventListener("click", function(){
-  document.querySelector(".pop-up-box").style.display = "flex";
+  document.querySelector(".pop-up-box").classList.add('active')
  });
