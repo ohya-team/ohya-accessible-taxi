548ce8eedda179b61135b6f34e6cb3e6a492f7a6
yushinhe <robin124708476@gmail.com>
2021-06-29 13:00:32 +0800

    改檔名

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,8 @@ module.exports = {
         programList: './dev/js/programList.js',
         travelFormTwo: './dev/js/travelFormTwo.js',
         forumDetail:'./dev/js/forumDetail.js',
+        jqueryMin:'./dev/js/jqueryMin.js',
+        jquerySliderPro:'./dev/js/jquerySliderPro.js',
     },// 入口文件
     output: {
         path: path.resolve(__dirname, 'dist/js'),
