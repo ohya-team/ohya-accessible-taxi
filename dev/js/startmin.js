6890ce93c77cea11e74cc8c6dedc4b8a7ce3b86a
ohya-team <tibameric@gmail.com>
2021-06-29 15:53:28 +0800

    admin-login

---
+++ dev/js/startmin.js
@@ -0,0 +1,91 @@
+$(function () {
+
+  $('#side-menu').metisMenu();
+
+});
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function () {
+  $(window).bind("load resize", function () {
+    topOffset = 50;
+    width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+    if (width < 768) {
+      $('div.navbar-collapse').addClass('collapse');
+      topOffset = 100; // 2-row-menu
+    } else {
+      $('div.navbar-collapse').removeClass('collapse');
+    }
+
+    height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+    height = height - topOffset;
+    if (height < 1) height = 1;
+    if (height > topOffset) {
+      $("#page-wrapper").css("min-height", (height) + "px");
+    }
+  });
+
+  var url = window.location;
+  var element = $('ul.nav a').filter(function () {
+    return this.href == url || url.href.indexOf(this.href) == 0;
+  }).addClass('active').parent().parent().addClass('in').parent();
+  if (element.is('li')) {
+    element.addClass('active');
+  }
+});
+
+
+// adminManage
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  var actions = $("table td:last-child").html();
+  // Append table with add row form on add new button click
+  $(".add-new").click(function () {
+    $(this).attr("disabled", "disabled");
+    var index = $("table tbody tr:last-child").index();
+    var row = '<tr>' +
+      '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+      '<td><input type="text" class="form-control" name="department" id="department"></td>' +
+      '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
+      '<td>' + actions + '</td>' +
+      '</tr>';
+    $("table").append(row);
+    $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+  // Add row on add button click
+  $(document).on("click", ".add", function () {
+    var empty = false;
+    var input = $(this).parents("tr").find('input[type="text"]');
+    input.each(function () {
+      if (!$(this).val()) {
+        $(this).addClass("error");
+        empty = true;
+      } else {
+        $(this).removeClass("error");
+      }
+    });
+    $(this).parents("tr").find(".error").first().focus();
+    if (!empty) {
+      input.each(function () {
+        $(this).parent("td").html($(this).val());
+      });
+      $(this).parents("tr").find(".add, .edit").toggle();
+      $(".add-new").removeAttr("disabled");
+    }
+  });
+  // Edit row on edit button click
+  $(document).on("click", ".edit", function () {
+    $(this).parents("tr").find("td:not(:last-child)").each(function () {
+      $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+    });
+    $(this).parents("tr").find(".add, .edit").toggle();
+    $(".add-new").attr("disabled", "disabled");
+  });
+  // Delete row on delete button click
+  $(document).on("click", ".delete", function () {
+    $(this).parents("tr").remove();
+    $(".add-new").removeAttr("disabled");
+  });
+});
