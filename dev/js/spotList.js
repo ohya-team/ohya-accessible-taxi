c046ca7d9162d34995e8f270bcb558eff3f4c54d
yushinhe <robin124708476@gmail.com>
2021-07-07 03:42:46 +0800

    增加頁籤

--- dev/js/spotList.js
+++ dev/js/spotList.js
@@ -6,7 +6,9 @@ let vue = new Vue({
         return {
             info: null,
             type: '所有景點',
-            typeList: ['所有景點', '風景區', '商圈', '藝文區']
+            perPage: 12,
+            typeList: ['所有景點', '風景區', '商圈', '藝文區'],
+            targetPageId: null,            
         }
     },
     mounted() {
@@ -15,19 +17,29 @@ let vue = new Vue({
             .catch(function (error) { // 请求失败处理
                 console.log(error);
             });
+        this.get_href();
     },
     computed: {
         filterData() {
             if (this.type === '所有景點') {
                 return this.info
-            } else {             
+            } else {
                 return this.info.filter(item => item.SPOT_CAT == this.type)
             }
+        },
+        totalPage(){
+          return  parseInt(this.filterData.length / this.perPage)+1 ;
         }
     },
     methods: {
-        changeList(item) {             
+        changeList(item) {
             this.type = item;
-        }
+        },
+        get_href() {
+            let nowUrl = window.location.href;
+            let targetPageId = nowUrl.split("=")[1];
+            console.log(targetPageId);
+            this.targetPageId = parseInt(targetPageId);
+        }        
     }
 })
