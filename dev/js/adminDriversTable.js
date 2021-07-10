1c58c38f2e35a7f8eb444a6af508477a85a6f8a0
api810209 <sp810209@gmail.com>
2021-07-10 22:52:07 +0800

    司機排班帶資料測試成功

--- dev/js/adminDriversTable.js
+++ dev/js/adminDriversTable.js
@@ -1,19 +1,13 @@
 import Vue from "vue";
+import axios from "axios";
 
 let vm =  new Vue({
     el: '#app',
     data: {
-       driverTable:[
-           {DRIVER_NAME:'郭阿明', DRIVER_PHONE:'0912345678', TAXI_LICENCENO:'AA1234'},
-           {DRIVER_NAME:'張阿明', DRIVER_PHONE:'0922345678', TAXI_LICENCENO:'BB1234'},
-           {DRIVER_NAME:'楊阿明', DRIVER_PHONE:'0932345678', TAXI_LICENCENO:'CC1234'},
-           {DRIVER_NAME:'黃阿明', DRIVER_PHONE:'0942345678', TAXI_LICENCENO:'DD1234'},
-       ]
+       driverTable:[],
     },
-    methods: {
-        
-    },
-    computed: {
-        
-    },
+    mounted() {
+        //   axios.get('../php/driver.php').then(res => console.log(res.data));
+          axios.get('../php/driver.php').then(res => this.driverTable = res.data);
+      },
 })
