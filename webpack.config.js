cb9655ef12b95c4f519a35ac8d1da7af705bc404
yushinhe <robin124708476@gmail.com>
2021-07-10 19:03:37 +0800

    webpack幫忙補

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
+        adminDriversTable:'./dev/js/adminDriversTable.js',
         bannerSlider: './dev/js/bannerSlider.js',
         booking: './dev/js/booking.js',
         forum: './dev/js/forum.js',
