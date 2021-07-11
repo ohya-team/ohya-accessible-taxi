097d44ff4ae463cebd8e6193b857e0c65b9830e6
api810209 <sp810209@gmail.com>
2021-07-11 13:44:42 +0800

    後台司機排班查詢功能 完成

--- dev/js/adminDriversTable.js
+++ dev/js/adminDriversTable.js
@@ -4,10 +4,28 @@ import axios from "axios";
 let vm =  new Vue({
     el: '#app',
     data: {
-       driverTable:[],
+        driverName: '',
+        driverPhone: '',
+        driverTable: [],
     },
     mounted() {
-        //   axios.get('../php/driver.php').then(res => console.log(res.data));
-          axios.get('../php/driver.php').then(res => this.driverTable = res.data);
-      },
+        axios.get('../php/driver.php').then(res => this.driverTable = res.data).catch(error => console.log(error));
+    },
+    methods: {
+        findName(){
+            for(let i=0; i<this.driverTable.length; i++){
+                if(this.driverName == this.driverTable[i].DRIVER_NAME || this.driverPhone == this.driverTable[i].DRIVER_PHONE){
+                    this.driverTable = [{
+                        DRIVER_NAME:this.driverTable[i].DRIVER_NAME,
+                        DRIVER_PHONE:this.driverTable[i].DRIVER_PHONE,
+                    }]
+                }
+            }
+            this.driverName = '';
+            this.driverPhone = '';
+        },
+        showAll(){
+            this.$mount('#app');
+        }
+    },
 })
