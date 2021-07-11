f06290b39663deaa74bcfdf853a86a90fb06db3a
api810209 <sp810209@gmail.com>
2021-07-11 21:16:31 +0800

    傻眼貓咪 組件亂跑內

--- dev/js/adminDriverRoster.js
+++ dev/js/adminDriverRoster.js
@@ -4,30 +4,12 @@ import axios from "axios";
 let vm =  new Vue({
     el: '#app',
     data: {
-        driverName: '',
-        driverPhone: '',
         driverTable: [],
+        driverRoster:[],
     },
     mounted() {
         axios.get('../php/driver.php').then(res => this.driverTable = res.data).catch(error => console.log(error));
-    },
-    methods: {
-        findName(){
-            for(let i=0; i<this.driverTable.length; i++){
-                //每一筆比對輸入的內容是否有吻合
-                if(this.driverName == this.driverTable[i].DRIVER_NAME || this.driverPhone == this.driverTable[i].DRIVER_PHONE){
-                    this.driverTable = [{
-                        DRIVER_NO:this.driverTable[i].DRIVER_NO,
-                        DRIVER_NAME:this.driverTable[i].DRIVER_NAME,
-                        DRIVER_PHONE:this.driverTable[i].DRIVER_PHONE,
-                    }]
-                }
-            }
-            this.driverName = '';
-            this.driverPhone = '';
-        },
-        showAll(){
-            this.$mount('#app');//回到mounted前
-        }
+
+        axios.get('../php/bookingTiming.php').then(res => this.driverRoster = res.data).catch(error => console.log(error));
     },
 })
