761ff79ecbcac5f76fae78b0a3687c0f0be84be3
api810209 <sp810209@gmail.com>
2021-07-12 16:44:42 +0800

    後台時段新增功能更新 可直接新增以天數計

--- dev/js/adminDriverRoster.js
+++ dev/js/adminDriverRoster.js
@@ -8,8 +8,19 @@ let vm =  new Vue({
         driverRoster:[],
     },
     mounted() {
-        axios.get('../php/driver.php').then(res => this.driverTable = res.data).catch(error => console.log(error));
+        axios.get('../php/driver.php')
+        .then(res => this.driverTable = res.data)
+        .catch(error => console.log(error))
 
-        axios.get('../php/bookingTiming.php').then(res => this.driverRoster = res.data).catch(error => console.log(error));
+        // axios.get('../php/bookingTiming.php')
+        // .then(res => this.driverRoster = res.data)
+        // .catch(error => console.log(error))
+
     },
 })
+
+// for(let i=0; i<vm.driverRoster.length; i++){    
+//     if(vm.driverRoster[i].DRIVER_NO == 1){
+//         drverTiming.push(vm.driverRoster[i].DRIVER_NO);              
+//     }
+// }
--- dev/php/insertBookingTiming.php
+++ dev/php/insertBookingTiming.php
@@ -1,11 +1,12 @@
 <?php
 require_once "../connect_cfd101g2.php";
 
-for($i=1; $i<11; $i++){
-    $sql = "INSERT INTO `booking_timing`(`DRIVER_NO`, `BOOKING_DATE`, `BOOKING_MORNING`, `BOOKING_AFTERNOON`, `BOOKING_EVENING`) VALUES ($i,:insertDates,1,1,1)";
-    $program = $pdo->prepare($sql);
-    $program->bindValue(":insertDates", $_GET["insertDates"]);
-    $program->execute();
+for($days=1; $days<=$_GET["insertDates"]; $days++){
+    for($i=1; $i<11; $i++){//i是司機人數
+        $sql = "INSERT INTO `booking_timing`(`DRIVER_NO`, `BOOKING_DATE`, `BOOKING_MORNING`, `BOOKING_AFTERNOON`, `BOOKING_EVENING`) VALUES ($i,adddate(curdate(),7+$days),1,1,1);";
+        $program = $pdo->prepare($sql);
+        $program->execute();
+    }
 }
 
 header('Location:../admin/adminBookingTiming.html');
