6a2b370f082c215ec8662845b6d9057284484ff2
gordonkuoss <gordonkuoss@gmail.com>
2021-07-13 12:23:10 +0800

    修改

---
+++ dev/php/usercenterTravel.php
@@ -0,0 +1,10 @@
+<?php
+try{
+	require_once("../connect_cfd101g2.php");
+	$sql = "select * from travel_order";
+	$products = $pdo->query($sql);
+	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($prodRows);
+}catch(PDOException $e){
+	//echo $e->getMessage();
+	exit("系統暫時不能提供服務");}
