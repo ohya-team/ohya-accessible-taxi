9f35ae2f2ea2fe5dcbd19426e92f57f85b5b14ac
yushinhe <robin124708476@gmail.com>
2021-07-14 19:15:51 +0800

    header

--- dev/php/member.php
+++ dev/php/member.php
@@ -1,18 +1,19 @@
 <?php
 ob_start();
 session_start();
-try{
-	require_once("../connect_cfd101g2.php");
-	$sql = "select * from member where mem_account=:mem_account and mem_password=:mem_password";
-	$member = $pdo->prepare($sql);
-	$member -> bindValue(":mem_account" , $_SESSION["mem_account"]);
-	$member -> bindValue(":mem_password" , $_SESSION["mem_password"]);
-  $member -> execute();
+try {
+    require_once "../connect_cfd101g2.php";
+    $memInfos =[];
+    if (isset($_SESSION["mem_account"])) {
+        $sql = "select * from member where mem_account=:mem_account and mem_password=:mem_password";
+        $member = $pdo->prepare($sql);
+        $member->bindValue(":mem_account", $_SESSION["mem_account"]);
+        $member->bindValue(":mem_password", $_SESSION["mem_password"]);
+        $member->execute();
+        $memInfos = $member->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-	$memInfos = $member->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($memInfos);
-}catch(PDOException $e){
-	echo $e->getMessage();
-	exit("系統暫時不能提供服務");}
-	?>
-
+    echo json_encode($memInfos);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit("系統暫時不能提供服務");}
