4fcbc22e0cdfdb1fb7f771f0c1c5573c437ac508
api810209 <sp810209@gmail.com>
2021-07-14 18:13:30 +0800

    前台一般預約撈資料

--- dev/js/booking2.js
+++ dev/js/booking2.js
@@ -4,13 +4,62 @@ import axios from "axios";
 let vm = new Vue({
     el: '#app',
     data: {
+        bookingInfo: [],
         checkTime:'確認時間',
         checkCar:'確認車種',
         checkDriver:'確認司機',
         checkForm:'訂單確認',
-        leftDate:new Date().getMonth()+1+'/'+new Date().getDate(),
+        curMonth: new Date().getMonth(),//index: 6+1月
+        // curDay: new Date().getDate(),//14號
+        curYear: new Date().getFullYear(),//2021年
+        fulldays: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        leftDay: new Date().getDate()+7,
+        rightDay: new Date().getDate()+13,
+        leftMonth: '',
+        rightMonth: '',
+        curweekDay: new Date().getDay(),
+        deskCurweekDays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],//抓中文星期用的
+        MobileCurweekDays: ['日', '一', '二', '三', '四', '五', '六'],//抓中文星期用的
+        tableTh:[],//實際要放日曆用的
     },
     mounted() {
+        //撈資料-步驟一到步驟三用
+        axios.get('../php/booking.php')
+        .then(res => this.bookingInfo = res.data)
+        .catch(error => console.log(error))
+
+        //處理閏年是29天
+        if(this.curYear%4 ==0){
+            this.fulldays[1]=29;
+        }
+
+        //處理7~15天後大於這個月的天數，要跳下一個月
+        if(this.leftDay>this.fulldays[this.curMonth]){
+            this.leftDay -=this.fulldays[this.curMonth];
+            this.leftMonth = this.curMonth+1;
+        }else{
+            this.leftMonth = this.curMonth;
+        }
         
+        if(this.rightDay>this.fulldays[this.curMonth]){
+            this.rightDay -= this.fulldays[curMonth];
+            this.rightMonth = this.curMonth+1;
+        }else{
+            this.rightMonth = this.curMonth;
+        }
+
+        //處理Table th+td
+        for(let i=0; i<this.deskCurweekDays.length; i++){
+            let j = (this.curweekDay+i) % 7;
+            this.tableTh.push({'deskWeek':this.deskCurweekDays[j],'MobileWeek':this.MobileCurweekDays[j],'month':this.leftMonth+1,'day':this.leftDay+i});
+        }
+    },
+    computed: {
+        btnStatus(){
+            return {
+                'btn-bookingTime': true,
+                // 'btn-bookingFull': this.checkTime == '確認時間',
+            };
+        }
     },
 });
