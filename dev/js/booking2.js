2304c98cbbdb9089b1b2aa5725e247ff5f8ebd9f
api810209 <sp810209@gmail.com>
2021-07-14 22:09:57 +0800

    booking按鈕抓剩餘司機人數

--- dev/js/booking2.js
+++ dev/js/booking2.js
@@ -4,13 +4,12 @@ import axios from "axios";
 let vm = new Vue({
     el: '#app',
     data: {
-        bookingInfo: [],
+        bookingInfo: [],//php/booking.php
         checkTime:'確認時間',
         checkCar:'確認車種',
         checkDriver:'確認司機',
         checkForm:'訂單確認',
         curMonth: new Date().getMonth(),//index: 6+1月
-        // curDay: new Date().getDate(),//14號
         curYear: new Date().getFullYear(),//2021年
         fulldays: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
         leftDay: new Date().getDate()+7,
@@ -20,11 +19,11 @@ let vm = new Vue({
         curweekDay: new Date().getDay(),
         deskCurweekDays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],//抓中文星期用的
         MobileCurweekDays: ['日', '一', '二', '三', '四', '五', '六'],//抓中文星期用的
-        tableTh:[],//實際要放日曆用的
+        tableTh: [],//實際要放日曆用的
     },
     mounted() {
         //撈資料-步驟一到步驟三用
-        axios.get('../php/booking.php')
+        axios.get('php/booking.php')
         .then(res => this.bookingInfo = res.data)
         .catch(error => console.log(error))
 
@@ -48,11 +47,69 @@ let vm = new Vue({
             this.rightMonth = this.curMonth;
         }
 
-        //處理Table th+td
+        //處理Table th+td & 個位數月日前面要+'0'
         for(let i=0; i<this.deskCurweekDays.length; i++){
             let j = (this.curweekDay+i) % 7;
-            this.tableTh.push({'deskWeek':this.deskCurweekDays[j],'MobileWeek':this.MobileCurweekDays[j],'month':this.leftMonth+1,'day':this.leftDay+i});
+
+            if((this.leftMonth+1)>9 && (this.leftDay+i)>9){
+                this.tableTh.push({
+                    'deskWeek':this.deskCurweekDays[j],
+                    'MobileWeek':this.MobileCurweekDays[j],
+                    'month':this.leftMonth+1,
+                    'day':this.leftDay+i
+                });
+            }else if((this.leftMonth+1)>9 && (this.leftDay+i)<=9){
+                this.tableTh.push({
+                    'deskWeek':this.deskCurweekDays[j],
+                    'MobileWeek':this.MobileCurweekDays[j],
+                    'month':this.leftMonth+1,
+                    'day':'0'+(this.leftDay+i)
+                });
+            }else if((this.leftMonth+1)<=9 && (this.leftDay+i)>9){
+                this.tableTh.push({
+                    'deskWeek':this.deskCurweekDays[j],
+                    'MobileWeek':this.MobileCurweekDays[j],
+                    'month':'0'+(this.leftMonth+1),
+                    'day':this.leftDay+i
+                });
+            }else{
+                this.tableTh.push({
+                    'deskWeek':this.deskCurweekDays[j],
+                    'MobileWeek':this.MobileCurweekDays[j],
+                    'month':'0'+(this.leftMonth+1),
+                    'day':'0'+(this.leftDay+i)
+                });
+            }
         }
+    },
+    methods: {
+        btnBookingMorning(m, d){
+            let count = 0;
+            for(let i in this.bookingInfo){
+                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i].BOOKING_MORNING==1){
+                    count +=1;
+                }
+            }
+            console.log(m,d,count);
+        },
+        btnBookingAfternoon(m, d){
+            let count = 0;
+            for(let i in this.bookingInfo){
+                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i].BOOKING_AFTERNOON==1){
+                    count +=1;
+                }
+            }
+            console.log(m,d,count);
+        },
+        btnBookingevening(m, d){
+            let count = 0;
+            for(let i in this.bookingInfo){
+                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i].BOOKING_AFTERNOON==1){
+                    count +=1;
+                }
+            }
+            console.log(m,d,count);
+        },
     },
     computed: {
         btnStatus(){
@@ -60,6 +117,7 @@ let vm = new Vue({
                 'btn-bookingTime': true,
                 // 'btn-bookingFull': this.checkTime == '確認時間',
             };
-        }
+        },
+        
     },
 });
