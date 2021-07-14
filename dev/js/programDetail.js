10239c83d120a22386a21ccb3f1efb60fa5e31b3
yushinhe <robin124708476@gmail.com>
2021-07-14 21:36:46 +0800

    改從programDetail進行確認

--- dev/js/programDetail.js
+++ dev/js/programDetail.js
@@ -9,8 +9,9 @@ let vue = new Vue({
     data() {
         return {
             info: null,
-            spotInfo:null,
-            targetPageId:null,
+            spotInfo: null,
+            targetPageId: null,
+            memInfo: null,
         }
     },
     mounted() {
@@ -19,20 +20,25 @@ let vue = new Vue({
             .then(response => (this.info = response.data))
             .catch(function (error) { // 请求失败处理
                 console.log(error);
-        });
+            });
         axios.get('./php/spotInProgram.php')
             .then(response => (this.spotInfo = response.data))
             .catch(function (error) { // 请求失败处理
                 console.log(error);
-        });
+            });
+        axios.get('./php/member.php')
+            .then(response => (this.memInfo = response.data))
+            .catch(function (error) { // 请求失败处理
+                console.log(error);
+            });
     },
-    computed:{
-        DetailInfo(){
+    computed: {
+        DetailInfo() {
             if (this.info != null) {
                 return this.info.filter(item => item.PROGRAM_NO == this.targetPageId)
             }
         },
-        spotDetailInfo(){
+        spotDetailInfo() {
             if (this.spotInfo != null) {
                 return this.spotInfo.filter(item => item.PROGRAM_NO == this.targetPageId)
             }
@@ -43,6 +49,14 @@ let vue = new Vue({
             let nowUrl = window.location.href;
             let targetPageId = nowUrl.split("=")[1];
             this.targetPageId = targetPageId;
+        },
+        order() {
+            if (this.memInfo.length > 0) {
+                location.href = './travelFormOne.html'
+            } else {
+                alert('請先登入再進行下單')
+                location.href = './login.html'
+            }
         }
     }
 })
