d97685ce3c78b0f457e07c46629653ba67cc83dd
api810209 <sp810209@gmail.com>
2021-07-15 18:06:40 +0800

    重覆車車資料篩選不出來 很崩潰

--- dev/js/booking2.js
+++ dev/js/booking2.js
@@ -20,6 +20,7 @@ let vm = new Vue({
         deskCurweekDays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],//抓中文星期用的
         MobileCurweekDays: ['日', '一', '二', '三', '四', '五', '六'],//抓中文星期用的
         tableTh: [],//實際要放日曆用的
+        CarInfo: [],//放步驟2用的
     },
     mounted() {
         //撈資料-步驟一到步驟三用
@@ -28,9 +29,7 @@ let vm = new Vue({
         .catch(error => console.log(error))
 
         //處理閏年是29天
-        if(this.curYear%4 ==0){
-            this.fulldays[1]=29;
-        }
+        this.curYear%4 ==0 ? this.fulldays[1]=29 : this.fulldays[1]=28
 
         //處理7~15天後大於這個月的天數，要跳下一個月
         if(this.leftDay>this.fulldays[this.curMonth]){
@@ -51,64 +50,33 @@ let vm = new Vue({
         for(let i=0; i<this.deskCurweekDays.length; i++){
             let j = (this.curweekDay+i) % 7;
 
-            if((this.leftMonth+1)>9 && (this.leftDay+i)>9){
-                this.tableTh.push({
-                    'deskWeek':this.deskCurweekDays[j],
-                    'MobileWeek':this.MobileCurweekDays[j],
-                    'month':this.leftMonth+1,
-                    'day':this.leftDay+i
-                });
-            }else if((this.leftMonth+1)>9 && (this.leftDay+i)<=9){
-                this.tableTh.push({
-                    'deskWeek':this.deskCurweekDays[j],
-                    'MobileWeek':this.MobileCurweekDays[j],
-                    'month':this.leftMonth+1,
-                    'day':'0'+(this.leftDay+i)
-                });
-            }else if((this.leftMonth+1)<=9 && (this.leftDay+i)>9){
-                this.tableTh.push({
-                    'deskWeek':this.deskCurweekDays[j],
-                    'MobileWeek':this.MobileCurweekDays[j],
-                    'month':'0'+(this.leftMonth+1),
-                    'day':this.leftDay+i
-                });
-            }else{
-                this.tableTh.push({
-                    'deskWeek':this.deskCurweekDays[j],
-                    'MobileWeek':this.MobileCurweekDays[j],
-                    'month':'0'+(this.leftMonth+1),
-                    'day':'0'+(this.leftDay+i)
-                });
-            }
+            this.tableTh.push({
+                'deskWeek':this.deskCurweekDays[j],
+                'MobileWeek':this.MobileCurweekDays[j],
+                'month':(this.leftMonth+1)<10 ? "0"+(this.leftMonth+1) : this.leftMonth+1,
+                'day':(this.leftDay+i)<10 ? "0"+(this.leftDay+i) : this.leftDay+i
+            });
         }
     },
     methods: {
-        btnBookingMorning(m, d){
-            let count = 0;
+        btnBooking(m, d, t){//確認點選按鈕的對應時段與司機尚餘幾位
+            let driverCount = 0;
+            let tempCarInfo = [];
+            
             for(let i in this.bookingInfo){
-                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i].BOOKING_MORNING==1){
-                    count +=1;
+                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i][t]==1){
+                    driverCount +=1;
+
+                    tempCarInfo.push({
+                        carNo:this.bookingInfo[i].CAR_NO,
+                        carPic:this.bookingInfo[i].CAR_PIC,
+                        carType:this.bookingInfo[i].CAR_TYPE,
+                        cardes:this.bookingInfo[i].CAR_DESCRIBE,
+                    });
                 }
             }
-            console.log(m,d,count);
-        },
-        btnBookingAfternoon(m, d){
-            let count = 0;
-            for(let i in this.bookingInfo){
-                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i].BOOKING_AFTERNOON==1){
-                    count +=1;
-                }
-            }
-            console.log(m,d,count);
-        },
-        btnBookingevening(m, d){
-            let count = 0;
-            for(let i in this.bookingInfo){
-                if(this.bookingInfo[i].BOOKING_DATE.split('-')[1] == m && this.bookingInfo[i].BOOKING_DATE.split('-')[2] == d && this.bookingInfo[i].BOOKING_AFTERNOON==1){
-                    count +=1;
-                }
-            }
-            console.log(m,d,count);
+
+            console.log(m,d,t,driverCount,tempCarInfo, this.carInfo);
         },
     },
     computed: {
