863ca97b099f798dd785f4c252e9a814514a5dfd
yushinhe <robin124708476@gmail.com>
2021-07-16 23:17:19 +0800

    usercenter調整

--- dev/js/userCenter.js
+++ dev/js/userCenter.js
@@ -1,3 +1,23 @@
+import Vue from "vue";
+import axios from "axios";
+let vue = new Vue({
+    el: "#app",
+    data() {
+        return {
+        memInfo: null,
+        }
+    },
+    mounted() {
+        axios.post('../dist/php/member.php')
+            .then((res) => {
+                this.memInfo = res.data;
+                console.log(res.data);
+            })
+            .catch( (error) => console.log(error));
+    },
+})
+
+
 //圖片瀏覽
 function showUserpic(){
   $('#imgInp').on('change' , function(){
