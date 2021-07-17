d991c0b8a5c05368bb768310b20c415e32b8ebfd
api810209 <sp810209@gmail.com>
2021-07-17 12:42:41 +0800

    四瓶負責的js新增vue模式設定

--- dev/js/adminDriversTable.js
+++ dev/js/adminDriversTable.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import axios from "axios";
 
+/*模式設定*/
+const isDebug_mode = process.env.NODE_ENV !== 'production';
+Vue.config.debug = isDebug_mode;
+Vue.config.devtools = isDebug_mode;
+
 let vm =  new Vue({
     el: '#app',
     data: {
