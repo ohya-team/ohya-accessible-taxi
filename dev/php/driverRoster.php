517671fb695741b5e61248b35f471c0c1680202d
api810209 <sp810209@gmail.com>
2021-07-18 17:20:46 +0800

    後台司機排班只可安排下個月的班表 完成

--- dev/php/driverRoster.php
+++ dev/php/driverRoster.php
@@ -3,7 +3,10 @@ try{
 	require_once("../connect_cfd101g2.php");
 	$sql = "SELECT b.SCH_NO, b.BOOKING_DATE, b.BOOKING_MORNING, b.BOOKING_AFTERNOON, b.BOOKING_EVENING, d.DRIVER_NO, d.DRIVER_NAME\n"
 
-    . "FROM booking_timing b NATURAL JOIN driver d;";
+    . "FROM booking_timing b NATURAL JOIN driver d\n"
+
+    . "WHERE month(b.BOOKING_DATE)=month(CURRENT_DATE);"; //本月
+    // . "WHERE month(b.BOOKING_DATE)=month(CURRENT_DATE)+1;"; //下月
 	$tbInfo = $pdo->query($sql);
 	$driverRoster = $tbInfo->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($driverRoster);
