3e5563c2ce10741daeaba68417fc1caf4da1d0f9
api810209 <sp810209@gmail.com>
2021-07-20 14:12:16 +0800

    帳密復原

--- dev/connect_cfd101g2.php
+++ dev/connect_cfd101g2.php
@@ -2,10 +2,8 @@
 try {
     $dbname = "tebamefe_cfd101g2";
 /*上傳用帳密，本機開發時請更改為個人mysql帳密*/
-    $user = "root";
-    $password = "root";
-    // $user = "tibamefe_since2021";
-    // $password = "vwRBSb.j&K#E";
+    $user = "tibamefe_since2021";
+    $password = "vwRBSb.j&K#E";
 
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
