48cbbb0712b85b8bc050865426678bb87a95d19c
api810209 <sp810209@gmail.com>
2021-07-24 17:39:38 +0800

    後台訂單訂單順序由訂單號從最新到最舊

--- dev/php/adminBookingForm.php
+++ dev/php/adminBookingForm.php
@@ -2,7 +2,7 @@
 try{
 	require_once("../connect_cfd101g2.php");
 
-    $sql = "SELECT f.*, m.MEM_LASTNAME,m.MEM_FIRSTNAME,m.MEM_PHONE,d.DRIVER_NAME,d.DRIVER_PHONE,c.CAR_TYPE,d.TAXI_LICENCENO FROM booking_form f JOIN member m JOIN car c JOIN driver d WHERE f.DRIVER_NO = d.DRIVER_NO and f.MEM_NO = m.MEM_NO and d.CAR_NO = c.CAR_NO;";
+    $sql = "SELECT f.*, m.MEM_LASTNAME,m.MEM_FIRSTNAME,m.MEM_PHONE,d.DRIVER_NAME,d.DRIVER_PHONE,c.CAR_TYPE,d.TAXI_LICENCENO FROM booking_form f JOIN member m JOIN car c JOIN driver d WHERE f.DRIVER_NO = d.DRIVER_NO and f.MEM_NO = m.MEM_NO and d.CAR_NO = c.CAR_NO ORDER BY f.ORDER_NO DESC;";
     
 	$bookingForm = $pdo->query($sql);
 	$formInfo = $bookingForm->fetchAll(PDO::FETCH_ASSOC);
